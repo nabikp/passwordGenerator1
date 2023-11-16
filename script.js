fad46c80c7a1c21a3ed1d27d1e43e2fb545e4807
Kalash Pratyay <91416760+nabikp@users.noreply.github.com>
2023-11-16 21:28:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbols = "!@#$%^&*(){}[]~+_=-";
+
+const allChars = uppercase + lowercase + number + symbols;
+
+function createPassword() {
+  let password = "";
+  password += uppercase[Math.floor(Math.random() * uppercase.length)];
+  password += lowercase[Math.floor(Math.random() * lowercase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbols[Math.floor(Math.random() * symbols.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passwordBox.value = password;
+}
+
+function copyPassword() {
+  passwordBox.select();
+  document.execCommand("copy");
+}
